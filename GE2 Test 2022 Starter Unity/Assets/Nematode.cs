1bd791b63b32522a38a4786e591ce283de8114cf
DylanConvery <dylanconvery94@gmail.com>
2022-04-26 10:16:00 +0100

    wip of nematodes

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -8,12 +8,25 @@ public class Nematode : MonoBehaviour
 
     public Material material;
 
+    public GameObject sphere;
+
+    private Vector3 scaleChange;
+
     void Awake()
     {
         // Put your code here!
+        length = Random.Range(2, 10);
+        scaleChange = new Vector3(0.1f, 0.1f, 0f);
+
+        for (var i = 0; i < length; i++)
+        {
+            GameObject segment = Instantiate(sphere, transform.position, transform.rotation, gameObject.transform);
+            segment.transform.localScale -= scaleChange;
+            scaleChange -= new Vector3(0.1f, 0.1f, 0);
+            segment.transform.position = transform.position - (transform.forward * i);
+        }
     }
-
-
+    
     // Start is called before the first frame update
     void Start()
     {
